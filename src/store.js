ad464594f999516049e79bd65285476f5455e8ce
Mike S <mstange@dthera.com>
2019-05-22 12:32:44 -0700

    linting

--- src/store.js
+++ src/store.js
@@ -1,42 +1,5 @@
 import { createStore } from 'redux';
-
-// initial state
-const initialState = {
-  a: 0,
-  b: 0,
-};
-
-// constants
-const SET_VALUE_A = 'SET_VALUE_A';
-const SET_VALUE_B = 'SET_VALUE_B';
-
-// actions
-export const setValueA = a => ({
-  type: SET_VALUE_A,
-  payload: {
-    a,
-  },
-});
-
-export const setValueB = b => ({
-  type: SET_VALUE_B,
-  payload: {
-    b,
-  },
-});
-
-// reducer
-const reducer = (state = initialState, action = {}) => {
-  // console.log(action);
-  switch (action.type) {
-    case SET_VALUE_A:
-      return { ...state, a: action.payload.a };
-    case SET_VALUE_B:
-      return { ...state, b: action.payload.b };
-    default:
-      return state;
-  }
-};
+import { reducer } from './redux/reducer';
 
 const store = createStore(reducer);
 
