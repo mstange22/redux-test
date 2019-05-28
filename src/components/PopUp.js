4aae7cebad0a872507da46d8bfa6772445d72a7c
Mike S <mstange@dthera.com>
2019-05-28 14:24:43 -0700

    implicit return

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PopUp = ({ title, message }) => {
-  console.log(title);
-  return (
-    <div
-      style={{
-        textAlign: 'center',
-        height: 100,
-        width: 400,
-        position:
-        'fixed',
-        top: 200,
-        left: '50%',
-        marginLeft: -220,
-        backgroundColor: '#f4f442',
-        border: 'solid 1px black',
-        borderRadius: 10,
-        display: 'flex',
-        justifyContent: 'flex-start',
-        padding: 20,
-        flexDirection: 'column',
-      }}
-    >
-      <div className="popup-title" style={{ fontWeight: 700 }}>
-        {`Pop-Up Says "${title}"`}
-      </div>
-      <br />
-      <div className="popup-message">
-        {message}
-      </div>
+const PopUp = ({ title, message }) => (
+  <div
+    style={{
+      textAlign: 'center',
+      height: 100,
+      width: 400,
+      position:
+      'fixed',
+      top: 200,
+      left: '50%',
+      marginLeft: -220,
+      backgroundColor: '#f4f442',
+      border: 'solid 1px black',
+      borderRadius: 10,
+      display: 'flex',
+      justifyContent: 'flex-start',
+      padding: 20,
+      flexDirection: 'column',
+    }}
+  >
+    <div className="popup-title" style={{ fontWeight: 700 }}>
+      {`Pop-Up Says "${title}"`}
     </div>
-  );
-};
+    <br />
+    <div className="popup-message">
+      {message}
+    </div>
+  </div>
+);
 
 PopUp.propTypes = {
   title: PropTypes.string.isRequired,
