e9fde4d724935b3af58b0c7b7d34e1767eb8100d
Mike S <mstange@dthera.com>
2019-05-28 16:22:11 -0700

    added tests to PopUp test

--- test/PopUp.test.js
+++ test/PopUp.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import PopUp from '../src/components/PopUp';
 
-const props = { title: 'Pop-Up Title', message: 'Your message goes here' };
+configure({ adapter: new Adapter() });
 
-test('changes the class when hovered', () => {
-  const component = renderer.create(
-    <PopUp {...props} />,
-  );
-  expect(component.toJSON()).toMatchSnapshot();
+const props = { title: 'Pop-Up Title', message: 'Your message goes here' };
+const shallowComponent = shallow(<PopUp {...props} />);
+
+describe('<Pop-Up />', () => {
+  test('snapshot test', () => {
+    const component = renderer.create(
+      <PopUp {...props} />,
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  test('exists', () => {
+    expect(shallowComponent.exists()).toBe(true);
+  });
+  test('title', () => {
+    expect(shallowComponent.find('.popup-title').text()).toBe(`Pop-Up Says "${props.title}"`);
+  });
+  test('message', () => {
+    expect(shallowComponent.find('.popup-message').text()).toBe(props.message);
+  });
 });
