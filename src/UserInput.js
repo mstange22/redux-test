1b478951fe43c95e34e4811370a74d734ea23068
Mike S <mstange@dthera.com>
2019-05-28 10:32:45 -0700

    change UserInput to pure function

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,55 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setValueA, setValueB } from './redux/reducer';
 
-class UserInput extends Component {
-
-  onChange = (e) => {
+const UserInput = (props) => {
+  const onChange = (e) => {
     const { name, value } = e.target;
     if (/^\d+$/.test(value) || value === '') {
-      if (name === 'a') this.props.setValueA(value === '' ? 0 : value);
-      else this.props.setValueB(value === '' ? 0 : value);
+      if (name === 'a') props.setValueA(value === '' ? 0 : value);
+      else props.setValueB(value === '' ? 0 : value);
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="user-input-container">
-        <b>Redux Calculator</b>
+  return (
+    <div className="user-input-container">
+      <b>Redux Calculator</b>
+      <br />
+      Enter two values
+      <div className="input-container">
+        <input
+          className="user-input"
+          name="a"
+          onChange={onChange}
+          value={props.a}
+          autoComplete="off"
+          autoFocus
+        />
         <br />
-        Enter two values
-        <div className="input-container">
-          <input
-            className="user-input"
-            name="a"
-            onChange={this.onChange}
-            value={this.props.a}
-            autoComplete="off"
-            autoFocus
-          />
-          <br />
-          <input
-            className="user-input"
-            name="b"
-            onChange={this.onChange}
-            value={this.props.b}
-            autoComplete="off"
-          />
-        </div>
+        <input
+          className="user-input"
+          name="b"
+          onChange={onChange}
+          value={props.b}
+          autoComplete="off"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 UserInput.propTypes = {
   a: PropTypes.number.isRequired,
   b: PropTypes.number.isRequired,
+  setValueA: PropTypes.func.isRequired,
+  setValueB: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  a: parseInt(state.a),
-  b: parseInt(state.b),
+  a: parseInt(state.a, 10),
+  b: parseInt(state.b, 10),
 });
 
 const mapDispatchToProps = {
