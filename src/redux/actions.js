e0dbc6f1b38be80c79c0c424b2c6a3618c0f6f0a
Mike S <mstange@dthera.com>
2019-05-28 12:18:04 -0700

    remove payload for single properties

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,14 +2,10 @@ import * as types from './constants';
 
 export const setValueA = a => ({
   type: types.SET_VALUE_A,
-  payload: {
-    a,
-  },
+  a,
 });
 
 export const setValueB = b => ({
   type: types.SET_VALUE_B,
-  payload: {
-    b,
-  },
+  b,
 });
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,9 +4,9 @@ import { initialState } from './initialState';
 export const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case types.SET_VALUE_A:
-      return { ...state, a: action.payload.a };
+      return { ...state, a: action.a };
     case types.SET_VALUE_B:
-      return { ...state, b: action.payload.b };
+      return { ...state, b: action.b };
     default:
       return state;
   }
